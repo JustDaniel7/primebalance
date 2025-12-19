24ca8dc7fcd58373ac9feedb4858bc13ada5c285
Daniel Sangermann <daniel.sangermann@hotmail.de>
2025-12-19 19:09:30 +0100

    x

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,21 @@
 'use client'
+// src/app/layout.tsx
+import { DM_Sans, Space_Grotesk, JetBrains_Mono } from 'next/font/google'
 
+const dmSans = DM_Sans({ 
+  subsets: ['latin'],
+  variable: '--font-dm-sans'
+})
+
+const spaceGrotesk = Space_Grotesk({ 
+  subsets: ['latin'],
+  variable: '--font-space-grotesk'
+})
+
+const jetbrainsMono = JetBrains_Mono({ 
+  subsets: ['latin'],
+  variable: '--font-jetbrains-mono'
+})
 import { DashboardLayout } from '@/components/layout'
 
 export default function DashboardRootLayout({
